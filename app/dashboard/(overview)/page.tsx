83a5d960b4fbf7a786d24e1a821a58742b10a155
Igor Morozov <im.energo@gmail.com>
2025-11-14 13:33:44 +0100

    removed unused imports

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -2,11 +2,7 @@ import { Card } from "@/app/ui/dashboard/cards";
 import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 import { lusitana } from "@/app/ui/fonts";
-import {
-  // fetchRevenue,
-  fetchLatestInvoices,
-  fetchCardData,
-} from "@/app/lib/data";
+import { fetchCardData } from "@/app/lib/data";
 import { Suspense } from "react";
 import {
   RevenueChartSkeleton,
@@ -14,7 +10,6 @@ import {
 } from "@/app/ui/skeletons";
 
 export default async function Page() {
-  // const revenue = await fetchRevenue();
   const {
     totalPaidInvoices,
     totalPendingInvoices,
