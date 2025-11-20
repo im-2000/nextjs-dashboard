21f29ae6a0f530eea22020e1b61cf659a03d4123
Igor Morozov <im.energo@gmail.com>
2025-11-20 11:44:25 +0100

    removed return in actions

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -33,9 +33,6 @@ export async function createInvoice(formData: FormData) {
     `;
   } catch (error) {
     console.error(error);
-    return {
-      message: "Database Error: Failed to Create Invoice.",
-    };
   }
 
   revalidatePath("/dashboard/invoices");
@@ -64,7 +61,6 @@ export async function updateInvoice(id: string, formData: FormData) {
     `;
   } catch (error) {
     console.error(error);
-    return { message: "Database Error: Failed to Update Invoice." };
   }
 
   revalidatePath("/dashboard/invoices");
